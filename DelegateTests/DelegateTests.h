8703c095118ece628c4e27686e102d630ba5ff26
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-08-23 11:22:32 -0300

    Initial Commit

---
+++ DelegateTests/DelegateTests.h
@@ -0,0 +1,13 @@
+//
+//  DelegateTests.h
+//  DelegateTests
+//
+//  Created by Rafael Brigagão Paulino on 23/08/12.
+//  Copyright (c) 2012 Rafael Brigagão Paulino. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DelegateTests : SenTestCase
+
+@end
---
+++ DelegateTests/DelegateTests.m
@@ -0,0 +1,32 @@
+//
+//  DelegateTests.m
+//  DelegateTests
+//
+//  Created by Rafael Brigagão Paulino on 23/08/12.
+//  Copyright (c) 2012 Rafael Brigagão Paulino. All rights reserved.
+//
+
+#import "DelegateTests.h"
+
+@implementation DelegateTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DelegateTests");
+}
+
+@end
